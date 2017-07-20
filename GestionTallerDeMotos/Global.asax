3343fb99c3f260da16c9dea29a67f54dfd6dd0be
Juan Moris <moris_juan@hotmail.com>
2017-07-20 09:49:00 -0400

    Agregar archivos de proyecto.

---
+++ GestionTallerDeMotos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionTallerDeMotos.MvcApplication" Language="C#" %>
---
+++ GestionTallerDeMotos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionTallerDeMotos.Startup))]
+namespace GestionTallerDeMotos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
