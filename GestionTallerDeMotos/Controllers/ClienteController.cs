e002391d5d4ef38ac455dc98406ad107979596a0
Juan Moris <moris_juan@hotmail.com>
2017-07-28 20:04:24 -0400

    Creación de Nuevo Cliente y Validación del Formulario.

--- GestionTallerDeMotos/Controllers/ClienteController.cs
+++ GestionTallerDeMotos/Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
 ﻿using GestionTallerDeMotos.Models;
+using GestionTallerDeMotos.Models.ModelosDeDominio;
 using GestionTallerDeMotos.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,10 +20,16 @@ namespace GestionTallerDeMotos.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
+            return View("ListaDeClientes");
+        }
+
+
+        public ActionResult NuevoCliente()
+        {
             var tiposDocumentos = _context.TiposDocumento.ToList();
             var personerias = _context.Personerias.ToList();
 
-            var viewModel = new ClienteViewModel
+            var viewModel = new ClienteViewModel()
             {
                 TiposDocumentos = tiposDocumentos,
                 Personerias = personerias
@@ -31,6 +38,30 @@ namespace GestionTallerDeMotos.Controllers
             return View("ClienteFormulario", viewModel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GuardarCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new ClienteViewModel(cliente)
+                {
+                    Personerias = _context.Personerias.ToList(),
+                    TiposDocumentos = _context.TiposDocumento.ToList()
+                };
+
+                return View("ClienteFormulario", viewModel);
+            }
+
+            if (cliente.Id == 0) {
+                cliente.FechaDeIngreso = DateTime.Now;
+                _context.Clientes.Add(cliente);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
 
     }
 }
--- GestionTallerDeMotos/Models/ModelosDeDominio/Cliente.cs
+++ GestionTallerDeMotos/Models/ModelosDeDominio/Cliente.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GestionTallerDeMotos.Models.ModelosDeDominio
 {
@@ -6,30 +7,39 @@ namespace GestionTallerDeMotos.Models.ModelosDeDominio
     {
         public int Id { get; set; }
 
+        [Required]
         public string Nombre { get; set; }
 
         public string Apellido { get; set; }
 
         public string Telefono { get; set; }
 
+        [Display(Name = "Teléfono")]
         public string Direccion { get; set; }
 
+        [Display(Name = "Correo Electrónico")]
         public string CorreoElectronico { get; set; }
 
+        [Display(Name = "Propietario")]
         public string NombrePropietario { get; set; }
 
+        [Display(Name = "Fecha de Nacimiento")]
         public DateTime? FechaDeNacimiento { get; set; }
 
         public DateTime FechaDeIngreso { get; set; }
 
         public Personeria Personeria { get; set; }
 
+        [Display(Name = "Personería")]
         public byte PersoneriaId { get; set; }
 
         public TipoDocumento TipoDocumento { get; set; }
 
+        [Display(Name = "Tipo de Documento")]
         public byte TipoDocumentoId { get; set; }
 
+        [Required]
+        [Display(Name = "Valor del Documento")]
         public string ValorDocumento { get; set; }
     }
 }
