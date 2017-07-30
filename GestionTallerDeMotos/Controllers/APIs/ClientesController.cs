9dc301002c4466c235fc889aa6b7ed479eb2e943
Juan Moris <moris_juan@hotmail.com>
2017-07-29 20:05:24 -0400

    Creación de la API de Clientes para Obtener la Lista de Clientes con Web API.

---
+++ GestionTallerDeMotos/Controllers/APIs/ClientesController.cs
@@ -0,0 +1,32 @@
+﻿using GestionTallerDeMotos.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GestionTallerDeMotos.Controllers.APIs
+{
+    public class ClientesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public ClientesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObtenerClientes()
+        {
+            var clientes = _context.Clientes
+                .Include(c => c.Personeria)
+                .Include(c => c.TipoDocumento)
+                .ToList();
+
+            return Ok(clientes);
+        }
+    }
+}
