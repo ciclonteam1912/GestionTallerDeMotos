03dee36ec693f45a5d4f7cf6df070b54ea70e442
Juan Moris <moris_juan@hotmail.com>
2017-08-16 16:16:46 -0400

    Creación de Formulario para Crear Hoja de Presupuesto y sus Detalles.

---
+++ GestionTallerDeMotos/ViewModels/PresupuestoDetalleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using GestionTallerDeMotos.Models.ModelosDeDominio;
+using System.Collections.Generic;
+
+namespace GestionTallerDeMotos.ViewModels
+{
+    public class PresupuestoDetalleViewModel
+    {
+        public IEnumerable<Producto> Productos { get; set; }
+    }
+}
